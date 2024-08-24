b50e1ff6bc64e5134ae45a95f03e32c8d87fb097
Balram Chaudhary <42298794+balram20162053@users.noreply.github.com>
2024-08-24 21:58:09 +0530

    feat: Role based access has been added to use Api

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/world")
+    @PreAuthorize("hasRole('admin')")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World For Admin";
+    }
+
+    @GetMapping("/worldUser")
+    @PreAuthorize("hasRole('user')")
+    public String helloWorldUser() {
+        return "Hello World For User";
     }
 }
